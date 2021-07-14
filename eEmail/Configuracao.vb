db598c7a2b2b719ad01e90907f4e24f7d30c0596
Wanderson Caldas <wcaldasti@gmail.com>
2021-07-13 23:25:21 -0300

    CRIAR PROJETO

---
+++ eEmail/Configuracao.vb
@@ -0,0 +1,120 @@
+﻿Public Class Configuracao
+    Public Property cod_ssl() As Integer
+        Get
+            Return m_cod_ssl
+        End Get
+        Set(ByVal value As Integer)
+            m_cod_ssl = value
+        End Set
+    End Property
+    Private m_cod_ssl As Integer
+
+    Public Property cod_porta() As Integer
+        Get
+            Return m_cod_porta
+        End Get
+        Set(ByVal value As Integer)
+            m_cod_porta = value
+        End Set
+    End Property
+    Private m_cod_porta As Integer
+    Public Property txt_conta_email() As String
+        Get
+            Return m_txt_conta_email
+        End Get
+        Set(ByVal value As String)
+            m_txt_conta_email = value
+        End Set
+    End Property
+    Private m_txt_conta_email As String
+
+    Public Property txt_servidor_smtp() As String
+        Get
+            Return m_txt_servidor_smtp
+        End Get
+        Set(ByVal value As String)
+            m_txt_servidor_smtp = value
+        End Set
+    End Property
+    Private m_txt_servidor_smtp As String
+
+    Public Property txt_servidor_smtp_usuario() As String
+        Get
+            Return m_txt_servidor_smtp_usuario
+        End Get
+        Set(ByVal value As String)
+            m_txt_servidor_smtp_usuario = value
+        End Set
+    End Property
+    Private m_txt_servidor_smtp_usuario As String
+
+    Public Property txt_servidor_smtp_senha() As String
+        Get
+            Return m_txt_servidor_smtp_senha
+        End Get
+        Set(ByVal value As String)
+            m_txt_servidor_smtp_senha = value
+        End Set
+    End Property
+    Private m_txt_servidor_smtp_senha As String
+
+    Public Property txt_remetente() As String
+        Get
+            Return m_txt_remetente
+        End Get
+        Set(ByVal value As String)
+            m_txt_remetente = value
+        End Set
+    End Property
+    Private m_txt_remetente As String
+
+    Public Property txt_destinatario() As String
+        Get
+            Return m_txt_destinatario
+        End Get
+        Set(ByVal value As String)
+            m_txt_destinatario = value
+        End Set
+    End Property
+    Private m_txt_destinatario As String
+
+    Public Property txt_mensagem() As String
+        Get
+            Return m_txt_mensagem
+        End Get
+        Set(ByVal value As String)
+            m_txt_mensagem = value
+        End Set
+    End Property
+    Private m_txt_mensagem As String
+
+    Public Property txt_assunto() As String
+        Get
+            Return m_txt_assunto
+        End Get
+        Set(ByVal value As String)
+            m_txt_assunto = value
+        End Set
+    End Property
+    Private m_txt_assunto As String
+
+    Public Property txt_email_padrao() As String
+        Get
+            Return m_txt_email_padrao
+        End Get
+        Set(ByVal value As String)
+            m_txt_email_padrao = value
+        End Set
+    End Property
+    Private m_txt_email_padrao As String
+
+    Public Property txt_status() As String
+        Get
+            Return m_txt_status
+        End Get
+        Set(ByVal value As String)
+            m_txt_status = value
+        End Set
+    End Property
+    Private m_txt_status As String
+End Class
---
+++ eEmail/Module1.vb
@@ -0,0 +1,54 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim objConfiguracao As New Configuracao
+
+        'Cria objeto com dados do e-mail.
+        Dim objEmail As New System.Net.Mail.MailMessage()
+
+        'Define o remetente do e-mail.
+        objEmail.From = New System.Net.Mail.MailAddress(objConfiguracao.txt_servidor_smtp_usuario, objConfiguracao.txt_remetente, System.Text.Encoding.UTF8)
+
+        'Define os destinatários do e-mail.
+        objEmail.To.Add(objConfiguracao.txt_destinatario)
+
+        'Define a prioridade do e-mail.
+        objEmail.Priority = System.Net.Mail.MailPriority.Normal
+
+        'Define o formato do e-mail HTML (caso não queira HTML alocar valor false)
+        objEmail.IsBodyHtml = True
+
+        'Define o título do e-mail.
+        objEmail.Subject = objConfiguracao.txt_assunto
+
+        'Define o corpo do e-mail.        
+        objEmail.Body = objConfiguracao.txt_mensagem
+
+        'Para evitar problemas com caracteres "estranhos", configuramos o Charset para "ISO-8859-1"
+        objEmail.SubjectEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1")
+        objEmail.BodyEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1")
+
+        'Cria objeto com os dados do SMTP
+        Dim objSmtp As New System.Net.Mail.SmtpClient
+
+        'Alocamos o endereço do host para enviar os e-mails:
+        objSmtp.Host = objConfiguracao.txt_servidor_smtp
+        objSmtp.UseDefaultCredentials = False
+        objSmtp.Credentials = New System.Net.NetworkCredential(objConfiguracao.txt_servidor_smtp_usuario, objConfiguracao.txt_servidor_smtp_senha)
+        objSmtp.Port = objConfiguracao.cod_porta
+        objSmtp.EnableSsl = True
+
+        Dim erro As String = ""
+        'Enviamos o e-mail através do método .Send()
+        Try
+            objSmtp.Send(objEmail)
+        Catch ex As Exception
+            erro = ex.Source & "<br />" & ex.Message & "<br />" & ex.StackTrace
+        Finally
+            'Excluímos o objeto de e-mail da memória, é opcional
+            objEmail.Dispose()
+        End Try
+
+    End Sub
+
+End Module
